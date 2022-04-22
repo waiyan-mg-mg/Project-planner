15587be8f91f4773de9797508e7663e691a959e6
matrix <usergit436@gmail.com>
2022-04-22 22:56:24 +0630

    setUp database

---
+++ src/components/Project.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    class="project_container"
+    :class="{ done: inProgress }"
+    @click.self="toggleDetail = !toggleDetail"
+  >
+    <div class="flex_container" @click.self="toggleDetail = !toggleDetail">
+      <h4 @click.self="toggleDetail = !toggleDetail">{{ project.title }}</h4>
+      <div class="icon_box">
+        <span class="trash" @click="this.$emit('removeProject')">🗑</span>
+        <span class="edit">🖍</span>
+        <span
+          class="done"
+          @click="this.inProgress = !this.inProgress"
+          :class="{ doneStatus: inProgress }"
+          >✔</span
+        >
+      </div>
+    </div>
+    <p v-if="toggleDetail">{{ project.detail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Project_Com",
+  props: ["project"],
+  data() {
+    return {
+      toggleDetail: false,
+      inProgress: false,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.project_container {
+  padding: 15px 25px;
+  margin: 15px 0;
+  background: rgb(230, 230, 230);
+  border-radius: 10px;
+  cursor: pointer;
+  border-left: 3px solid red;
+}
+.project_container.done {
+  border-color: green;
+}
+.project_container h4 {
+  margin: 10px 0;
+  color: indigo;
+}
+.project_container p::selection {
+  user-select: none;
+  background-color: gray;
+  color: whitesmoke;
+}
+.flex_container {
+  user-select: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.icon_box {
+  cursor: auto;
+}
+.icon_box span {
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-left: 10px;
+  color: black;
+  cursor: pointer;
+  transition: transform 500ms;
+}
+.icon_box span:hover {
+  transition: all 500ms;
+  display: inline-block;
+  transform: translateY(-5px);
+}
+.icon_box span:active {
+  color: black;
+}
+.icon_box .trash {
+  color: crimson;
+}
+.icon_box .edit {
+  color: orangered;
+}
+.icon_box .done {
+  color: green;
+}
+.done.doneStatus {
+  color: gray;
+}
+.done.doneStatus:hover {
+  transform: translate(0px);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <Project :project="project" @removeProject="removeProject(project.id)" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Project from "../components/Project";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Project },
+  name: "HomeView",
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    const fetchProject = async () => {
+      const response = await fetch("http://localhost:3000/projects");
+      if (response.status !== 200) {
+        throw response;
+      }
+      const projectsArr = await response.json();
+      return projectsArr;
+    };
+    fetchProject()
+      .then((responseProjects) => (this.projects = responseProjects))
+      .catch((response) => console.log(response.statusText));
+  },
+  methods: {
+    removeProject(removeId) {
+      this.projects = this.projects.filter((single) => single.id !== removeId);
+    },
+  },
+};
 </script>
